6a0a826beab6f3ddb2ac8b517f5ebdc31da0218a
baraa <45298945+BaraaMadi@users.noreply.github.com>
2019-01-19 13:32:49 +0300

    JQuery For Color Option
    Show Color Option Div When Click On The Gear

--- js/plugin.js
+++ js/plugin.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
   $('.carousel').carousel({
      interval:6000  /* 6 min bettwen one slide to another*/
   });  
-    
+ 
+    //Show Color Option Div When Click On The Gear 
+    $(".gear-check").click(function(){
+       $(".color-option").fadeToggle(); 
+    });
 });
